7b1d0f8806e497efb2ed31829d22587cfc6c3ee5
David-Jiang <nanwork0914@gmail.com>
2018-07-06 18:14:49 +0800

    add keep Awake for heroku

--- stock/src/main/java/com/stock/service/ScheduledTasks.java
+++ stock/src/main/java/com/stock/service/ScheduledTasks.java
@@ -1,6 +1,12 @@
 package com.stock.service;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,6 +25,25 @@ public class ScheduledTasks {
 			openData.updateStockInfo();
 		} catch (Exception e) {
 			System.out.println("排程發生錯誤，原因為：" + e.getMessage());
+		}
+    }
+	
+	@Scheduled(cron = "0 0/10 * * * ?")
+    public void keepAwake() {
+		try {
+			URL url = new URL("https://astrology-linebot.herokuapp.com/");
+			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+		    conn.connect();
+		    
+		    BufferedReader buffer = new BufferedReader(new InputStreamReader(conn.getInputStream(), "MS950"));
+		    String line = "";
+		    while((line = buffer.readLine()) != null) {
+		    	line += "";
+		    }
+		    buffer.close();
+			conn.disconnect();
+		} catch (Exception e) {
+			System.out.println("keepAwake發生錯誤，原因為：" + e.getMessage());
 		}
     }
 	
